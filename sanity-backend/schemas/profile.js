8c5a8d8fd5d0d6f1c42d7b495876da36fc52c2bd
Palash <palashvishnani57@gmail.com>
2022-06-26 18:32:06 +0530

    My first portfolio-website commit

---
+++ sanity-backend/schemas/profile.js
@@ -0,0 +1,147 @@
+export default {
+    name: 'profile',
+    type: 'document',
+      title: 'Profile',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title'
+      },
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Name'
+      },
+      {
+        name: 'image',
+        type: 'image',
+        title: 'Image'
+      },
+      {
+        name: 'githublink',
+        type: 'string',
+        title: 'GithubLink'
+      },
+      {
+        name: 'linkedinlink',
+        type: 'string',
+        title: 'LinkedinLink'
+      },
+      {
+        name: 'instagramlink',
+        type: 'string',
+        title: 'InstagramLink'
+      },
+      {
+        name: 'metadesc',
+        type: 'string',
+        title: 'MetaDesc'
+      },
+      {
+        name: 'service1',
+        type: 'string',
+        title: 'Service1'
+      },
+      {
+        name: 'service2',
+        type: 'string',
+        title: 'Service2'
+      },
+      {
+        name: 'service3',
+        type: 'string',
+        title: 'Service3'
+      },
+      {
+        name: 'service4',
+        type: 'string',
+        title: 'Service4'
+      },
+      {
+        name: 'service5',
+        type: 'string',
+        title: 'Service5'
+      },
+      {
+        name: 'service6',
+        type: 'string',
+        title: 'Service6'
+      },
+      {
+        name: 'workimage1',
+        type: 'image',
+        title: 'WorkImage1'
+      },
+      {
+        name: 'workimage2',
+        type: 'image',
+        title: 'WorkImage2'
+      },
+      {
+        name: 'workimage3',
+        type: 'image',
+        title: 'WorkImage3'
+      },
+      {
+        name: 'workimage4',
+        type: 'image',
+        title: 'WorkImage4'
+      },
+      {
+        name: 'internimage1',
+        type: 'image',
+        title: 'InternImage1'
+      },
+      {
+        name: 'interndesc1',
+        type: 'string',
+        title: 'InternDesc1'
+      },
+      {
+        name: 'internimage2',
+        type: 'image',
+        title: 'InternImage2'
+      },
+      {
+        name: 'interndesc2',
+        type: 'string',
+        title: 'InternDesc2'
+      },
+      {
+        name: 'internimage3',
+        type: 'image',
+        title: 'InternImage3'
+      },
+      {
+        name: 'interndesc3',
+        type: 'string',
+        title: 'InternDesc3'
+      },
+      {
+        name: 'projectcount',
+        type: 'string',
+        title: 'ProjectCount'
+      },
+      {
+        name: 'awardcount',
+        type: 'string',
+        title: 'AwardCount'
+      },
+      {
+        name: 'bugscount',
+        type: 'string',
+        title: 'BugsCount'
+      },
+      {
+        name: 'phone',
+        type: 'string',
+        title: 'Phone'
+      },
+      {
+        name: 'emailid',
+        type: 'string',
+        title: 'EmailId'
+      },
+    ]
+}
